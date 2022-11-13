95a7f9470522a0989680330404284568580e3841
Akash <chavanakash12098@gmail.com>
2022-11-13 19:39:58 +0530

    json-data-in-tableformant

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://dummyjson.com/users').then((res) => {
+      console.log(res.data)
+      setState(res.data.users);
+      console.log(res.data.users);
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="table table-bordered">
+        <tr>
+          <th scope="col">firstName</th>
+          <th scope="col">lastName</th>
+          <th scope="col"> age</th>
+          <th scope="col"> bloodGroup</th>
+        </tr>
+        {
+          state.map(
+            ele => {
+              return (
+
+                <tr>
+
+                  <td>{ele.firstName}</td>
+                  <td>{ele.lastName}</td>
+                  <td>{ele.age}</td>
+                  <td>{ele.bloodGroup}</td>
+                </tr>
+
+              )
+            })
+        }
+      </table>
     </div>
+
   );
+
 }
 
 export default App;
